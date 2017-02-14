6bcabdf1ac89a5e44e9466407891838e3eded9c5
Matthew Scanlan <mtscanlan@gmail.com>
2017-02-13 23:41:15 -0500

    Initial Commit.

---
+++ StringPatternMatching/StringPatternMatching/Models/Result.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace StringPatternMatching {
+	public class Result {
+
+		public string product_name { get; set; }
+		public IEnumerable<Listing> listings { get; set; }
+
+		public Result(string name, IEnumerable<Listing> list) {
+			product_name = name;
+			listings = list;
+		}
+	}
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Camera Liting Pattern Matching
+Attempts to match cameras to advertised listings with multiple solutions. This is a retired work-in-progress started in mid 2015.
